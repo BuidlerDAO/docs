8dcf05bbcc5133ca43732c491f4a54d413c835ec
vadxq <dxl@vadxq.com>
2023-03-17 12:07:03 +0800

    feat: init docs

---
+++ src/consts.ts
@@ -0,0 +1,52 @@
+export const SITE = {
+	title: 'BuidlerDao Docs',
+	description: 'BuidlerDao docs.',
+	defaultLanguage: 'en-us',
+} as const;
+
+export const OPEN_GRAPH = {
+	image: {
+		src: 'https://cdn-fe.s3.amazonaws.com/static/website/logotext.svg',
+		alt:
+			'BuidlerDAO logo,' +
+			' with a purple saturn-like planet floating in the right foreground',
+	},
+	twitter: 'BuidlerDAO',
+};
+
+export const KNOWN_LANGUAGES = {
+	English: 'en',
+	简体中文: 'zh-cn',
+} as const;
+export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
+
+export const GITHUB_EDIT_URL = `https://github.com/BuidlerDAO/docs`;
+
+export const COMMUNITY_INVITE_URL = `https://discord.com/invite/Qx8GXtHUv2`;
+
+// See "Algolia" section of the README for more information.
+export const ALGOLIA = {
+	indexName: 'XXXXXXXXXX',
+	appId: 'XXXXXXXXXX',
+	apiKey: 'XXXXXXXXXX',
+};
+
+export type Sidebar = Record<
+	(typeof KNOWN_LANGUAGE_CODES)[number],
+	Record<string, { text: string; link: string }[]>
+>;
+export const SIDEBAR: Sidebar = {
+	en: {
+		'Intro': [
+			{ text: 'Introduction', link: 'en/introduction' },
+			{ text: 'Page 2', link: 'en/page-2' },
+			{ text: 'Page 3', link: 'en/page-3' },
+		],
+		"Frontend": []
+	},
+	'zh-cn': {
+		'介绍': [
+			{ text: 'Introduction', link: 'zh-cn/introduction' },
+		],
+	}
+};
